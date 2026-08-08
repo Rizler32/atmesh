82315ad4bc09959a8e926e71c24774cca15800de
Rizler32 <giacomo.rizza.2012@gmail.com>
2026-08-08 14:13:50 +0200

    Added UI to main.c

--- atmesh-app/main.c
+++ atmesh-app/main.c
@@ -1,5 +1,6 @@
 #include "atmesh-app/window.h"
 #include "atmesh-app/opengl.h"
+#include "atmesh-app/ui.h"
 
 #include <atmesh/log.h>
 #include <atil/platform/platform.h>
@@ -22,26 +23,36 @@ int main() {
         return 1;
     }
 
-    // bind opengl context
+    // bind opengl context and init
     window_bind_opengl_context(window);
 
     ATMESH_INFO("Window created successfully");
 
     opengl_init();
+    glClearColor(0,0,0,0);
     ATMESH_INFO("Initialized OpenGL");
 
-    glClearColor(0,0,0,0);
+    // create ui
+    ui_handle_t ui;
+    ui_create(window, &ui);
 
     while (!window_should_close(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        window_swap_buffers(window);
+        ui_begin_frame(ui);
+        ui_draw(ui);
+        ui_end_frame(ui);
         
+        window_swap_buffers(window);
         window_system_poll_events();
+
         atil_platform_sleep_ms(16);
     }
 
     // shutdown
+    ATMESH_INFO("Destroying ui");
+    ui_destroy(ui);
+    
     ATMESH_INFO("Destroying window");
     window_destroy(window);
     window_system_shutdown();
