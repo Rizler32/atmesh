451cff7827f46c66512335abbbf2242759283e25
Rizler32 <giacomo.rizza.2012@gmail.com>
2026-08-09 17:39:33 +0200

    Implemented file dialog for win32

---
+++ atmesh-app/src/atmesh-app/file_dialog_win32.c
@@ -0,0 +1,23 @@
+#ifdef _WIN32
+
+#include "atmesh-app/file_dialog.h"
+#include <windows.h>
+#include <commdlg.h>
+
+int open_filechooser(char *out_path, int size, bool save_mode) {
+    OPENFILENAME ofn;
+    ZeroMemory(&ofn, sizeof(ofn));
+
+    out_path[0] = '\0';
+
+    ofn.lStructSize = sizeof(ofn);
+    ofn.lpstrFile = out_path;
+    ofn.nMaxFile = size;
+    ofn.lpstrFilter = "Tutti i file\0*.*\0";
+    ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST;
+
+    return save_mode ? GetSaveFileName(&ofn)
+                     : GetOpenFileName(&ofn);
+}
+
+#endif // _WIN32
